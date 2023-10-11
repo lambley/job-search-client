5311fd5ad6088f6d61e1b6fa0363e1b7af63e40e
Aaron Lambley <aaron.lambley@gmail.com>
2023-10-11 13:14:49 +0100

    setup basic api call to getJobs endpoint

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -4,4 +4,25 @@ const jobSearchApi = axios.create({
   baseURL: "http://localhost:3000",
 });
 
+interface IGetJobsParams {
+  results_per_page: number;
+  what: string;
+  where: string;
+}
+
+const getJobs = async (params: IGetJobsParams): Promise<AxiosResponse> => {
+  const encodedWhat = encodeURIComponent(params.what);
+  const encodedWhere = encodeURIComponent(params.where);
+
+  const encodedParams = `?results_per_page=${params.results_per_page}&what=${encodedWhat}&where=${encodedWhere}`;
+
+  try {
+    const res = await jobSearchApi.get(`api/v1/jobs${encodedParams}`);
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
+
 export default jobSearchApi;
+export { getJobs };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,28 @@
 import React from "react";
+import { getJobs } from "./lib/api";
 
 export default function Home() {
+  const getRecentJobs = async (results_per_page, what, where) => {
+    try {
+      const res = await getJobs({ results_per_page, what, where });
+      console.log(res);
+      return res.data;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+
+  const renderJobs = async () => {
+    const jobs = await getRecentJobs(3, "javascript", "london");
+    return jobs.map((job) => (
+      <li key={job.id}>
+        <h3 className="text-xl font-semibold">{job.title}</h3>
+        <p>{job.description}</p>
+      </li>
+    ));
+  };
+
   return (
     <div className="min-h-screen flex flex-col">
       <h1 className="text-3xl font-bold text-center py-4">Home Page</h1>
@@ -9,7 +31,8 @@ export default function Home() {
         {/* Left Column */}
         <div className="w-1/2 p-4 m-10 rounded border shadow-md min-h-[20rem]">
           <h2 className="text-2xl">Recent jobs</h2>
-          <ul>
+          {renderJobs()}
+          {/* <ul>
             <li>
               <h3 className="text-xl font-semibold">Job 1</h3>
               <p>Job description</p>
@@ -22,7 +45,7 @@ export default function Home() {
               <h3 className="text-xl font-semibold">Job 3</h3>
               <p>Job description</p>
             </li>
-          </ul>
+          </ul> */}
         </div>
 
         {/* Right Column */}
