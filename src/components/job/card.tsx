000c30076d6f58dddb7d76d27742ea0a810c9ff2
Aaron Lambley <aaron.lambley@gmail.com>
2023-12-13 11:55:54 +0000

    update card layout

--- src/components/job/card.tsx
+++ src/components/job/card.tsx
@@ -2,7 +2,19 @@ import React from "react";
 import type { JobDbResponse } from "types/jobSearchApiTypes";
 
 export default function Card(props: JobDbResponse) {
-  const { id, title, description } = props;
+  const {
+    id,
+    title,
+    location,
+    description,
+    created,
+    company,
+    salary_min,
+    salary_max,
+    contract_type,
+    category,
+    processed_keywords,
+  } = props;
 
   const toTitleCase = (str: string) => {
     return str
@@ -11,10 +23,38 @@ export default function Card(props: JobDbResponse) {
       .join(" ");
   };
 
+  const numberToCurrency = (num: number) => {
+    return Math.trunc(num).toLocaleString("en-GB", {
+      style: "currency",
+      currency: "GBP",
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    });
+  };
+
   return (
     <div key={id}>
       <h3 className="text-xl my-1 font-semibold">{toTitleCase(title)}</h3>
-      <p className="ms-1">{description}</p>
+      <div className="ms-1">
+        <div>
+          <h4 className="my-1">
+            {numberToCurrency(salary_min)} / {contract_type || "Not Specified"}
+          </h4>
+          <h4 className="my-1">{company}</h4>
+        </div>
+        <div>
+          {created && (
+            <h4 className="my-1">
+              {new Date(created).toLocaleDateString("en-GB", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}
+            </h4>
+          )}
+        </div>
+        <p>{description}</p>
+      </div>
     </div>
   );
 }
