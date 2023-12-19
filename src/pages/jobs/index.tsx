f63a7c8b16140610fe07647dfc4044822d4ea97d
Aaron Lambley <aaron.lambley@gmail.com>
2023-12-19 09:29:12 +0000

    add refresh jobs button

--- src/pages/jobs/index.tsx
+++ src/pages/jobs/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { GetStaticProps } from "next";
 import Card from "@components/job/card";
 import TopKeywords from "@components/job/topKeywords";
-import { getJobs, getTopKeywords } from "@lib/api";
+import { getJobs, refreshJobs } from "@lib/api";
 import type { JobDbResponse } from "types/jobSearchApiTypes";
 import { emptyJobsResponse } from "@lib/emptyResponses";
 import { repeatElements } from "@lib/utils/arrayMethods";
@@ -13,9 +13,28 @@ type JobsProps = {
 };
 
 export default function Jobs({ jobs }: JobsProps) {
+  const handleRefreshJobs = async () => {
+    try {
+      const res = await refreshJobs({
+        results_per_page: 3,
+        what: "javascript",
+        where: "london",
+      });
+      const { data } = res.data;
+      console.log(data);
+    } catch (error) {}
+  };
+
   return (
     <div className="min-h-screen flex flex-col">
       <h1 className="text-3xl font-bold text-center py-4">Jobs Page</h1>
+
+      <button
+        onClick={handleRefreshJobs}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Refresh Jobs
+      </button>
 
       <div className="flex-1 flex justify-center items-center">
         {/* Left Column */}
@@ -32,7 +51,7 @@ export default function Jobs({ jobs }: JobsProps) {
 
         {/* Right Column */}
         <div className="w-1/2 p-4 m-10 rounded border shadow-md min-h-[20rem]">
-          <TopKeywords />
+          <TopKeywords/>
         </div>
       </div>
     </div>
