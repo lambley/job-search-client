65a2fd2884e2158ec99533cfd84bda53472b7236
Aaron Lambley <aaron.lambley@gmail.com>
2024-04-22 09:59:43 +0100

    update test dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -4,11 +4,16 @@ const config: Config = {
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: "coverage",
-  testEnvironment: 'jsdom',
+  testEnvironment: "jsdom",
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
   setupFilesAfterEnv: ["./jest.setup.ts"],
+  moduleNameMapper: {
+    "^@components/(.*)$": "<rootDir>/src/components/$1",
+    "^@lib/(.*)$": "<rootDir>/src/lib/$1",
+    "^types/(.*)$": "<rootDir>/src/types/$1",
+  },
 };
 
 export default config;
