0c1d79448b5f8c271a2916e4ad0a5b8ff029ea36
Aaron Lambley <aaron.lambley@gmail.com>
2024-04-24 09:33:18 +0100

    update index tests

--- src/pages/jobs/tests/index.test.tsx
+++ src/pages/jobs/tests/index.test.tsx
@@ -1,38 +1,35 @@
-import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import Jobs from '../index';
-import * as api from '@lib/api';
+import React from "react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import Jobs from "../index";
+import * as api from "@lib/api";
 import { emptyJobsResponse } from "@lib/emptyResponses";
 
-describe('Jobs component', () => {
-  it('renders without crashing', () => {
+describe("Jobs component", () => {
+  it("renders without crashing", () => {
     render(<Jobs jobs={[]} />);
-    expect(screen.getByText('Jobs Page')).toBeInTheDocument();
+    expect(screen.getByText("Jobs Page")).toBeInTheDocument();
   });
 
-  // TODO need to fix how jobs are mocked
-  // it('displays recent jobs correctly', async () => {
-  //   const mockRecentJobs = jest.fn().mockResolvedValue([emptyJobsResponse]);
+  it("displays recent jobs correctly", async () => {
+    const mockRecentJobs = jest.fn().mockResolvedValue([emptyJobsResponse]);
 
-  //   jest.spyOn(api, 'recentJobs').mockImplementation(mockRecentJobs);
+    jest.spyOn(api, "recentJobs").mockImplementation(mockRecentJobs);
 
-  //   render(<Jobs jobs={mockRecentJobs} />);
+    render(<Jobs jobs={[emptyJobsResponse]} />);
 
-  //   await waitFor(() => {
-  //     expect(screen.getByText('Job 1')).toBeInTheDocument();
-  //     expect(screen.getByText('Job 2')).toBeInTheDocument();
-  //     expect(screen.getByText('Job 3')).toBeInTheDocument();
-  //   });
-  // });
+    await waitFor(() => {
+      expect(screen.getByText(emptyJobsResponse.title)).toBeInTheDocument();
+    });
+  });
 
-  it('refreshes jobs when button is clicked', async () => {
+  it("refreshes jobs when button is clicked", async () => {
     const mockRefreshJobs = jest.fn().mockResolvedValue({});
 
-    jest.spyOn(api, 'refreshJobs').mockImplementation(mockRefreshJobs);
+    jest.spyOn(api, "refreshJobs").mockImplementation(mockRefreshJobs);
 
     render(<Jobs jobs={[]} />);
 
-    fireEvent.click(screen.getByText('Refresh Jobs'));
+    fireEvent.click(screen.getByText("Refresh Jobs"));
 
     await waitFor(() => {
       expect(mockRefreshJobs).toHaveBeenCalled();
