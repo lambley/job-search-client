46975d8694fd15811527d00a723a01d83157f2a9
Aaron Lambley <aaron.lambley@gmail.com>
2024-05-08 11:10:03 +0100

    update topkeywords tests

--- src/components/tests/job/topKeywords.test.tsx
+++ src/components/tests/job/topKeywords.test.tsx
@@ -5,23 +5,41 @@ import { getTopKeywords } from "@lib/api";
 
 jest.mock("@lib/api", () => ({
   getTopKeywords: jest.fn(() =>
-    Promise.resolve({ data: { data: ["keyword1", "keyword2", "keyword3"] } })
+    Promise.resolve({ data: { data: ["test1", "test2", "test3"] } })
   ),
 }));
 
 describe("TopKeywords Component", () => {
   it("renders hot keywords", async () => {
-    // Render the component
     const { getByText } = render(<TopKeywords />);
 
-    // Wait for the component to render
     await waitFor(() => expect(getByText("Hot Keywords")).toBeInTheDocument());
 
     await waitFor(() => {
-      // Assert that the mocked getTopKeywords function has been called
       expect(getTopKeywords).toHaveBeenCalled();
 
-      // Assert against the output of the mocked getTopKeywords function
+      expect(getByText("test1")).toBeInTheDocument();
+      expect(getByText("test2")).toBeInTheDocument();
+      expect(getByText("test3")).toBeInTheDocument();
+    });
+  });
+
+  it("fetches and renders top keywords", async () => {
+    render(<TopKeywords />);
+    await waitFor(() => expect(getTopKeywords).toHaveBeenCalledTimes(1));
+  });
+
+  it("handles error when fetching top keywords", async () => {
+    (getTopKeywords as jest.Mock).mockRejectedValueOnce(
+      new Error("Failed to fetch top keywords")
+    );
+
+    const { getByText } = render(<TopKeywords />);
+
+    await waitFor(() => {
+      expect(getTopKeywords).toHaveBeenCalled();
+
+      // renders the default keywords in the useState method
       expect(getByText("keyword1")).toBeInTheDocument();
       expect(getByText("keyword2")).toBeInTheDocument();
       expect(getByText("keyword3")).toBeInTheDocument();
